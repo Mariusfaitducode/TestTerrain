4f7e864791f5ac692fffc5f7dcbf4dddd2de136a
SkateZen <mariusmdm123@gmail.com>
2024-04-25 20:03:56 +0200

    clean code

---
+++ Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+public class MapGenerator : MonoBehaviour {
+
+	public enum DrawMode {NoiseMap, Mesh, FalloffMap};
+	public DrawMode drawMode;
+
+	public TerrainData terrainData;
+	public NoiseData noiseData;
+	public TextureData textureData;
+	
+	public Material terrainMaterial;
+	
+	[Range(0,6)]
+	public int editorPreviewLOD;
+	public bool autoUpdate;
+	float[,] falloffMap;
+
+	void Awake() {
+		textureData.ApplyToMaterial(terrainMaterial);
+		textureData.UpdateMeshHeights (terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
+	}
+	
+	void OnValuesUpdated() {
+		if (!Application.isPlaying) {
+			DrawMapInEditor ();
+		}
+	}
+	
+	void OnTextureValuesUpdated() {
+		textureData.ApplyToMaterial (terrainMaterial);
+	}
+
+	public int mapChunkSize {
+		get {
+			if (terrainData.useFlatShading) {
+				return 95;
+			} else {
+				return 239;
+			}
+		}
+	}
+
+	// Function called when generate button clicked
+	public void DrawMapInEditor() {
+		
+		// Texture update
+		textureData.UpdateMeshHeights (terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
+		
+		// Generate map data : PerlinNoise map and colour map with all parameters
+		MapData mapData = GenerateMapData (Vector2.zero);
+
+		MapDisplay display = FindObjectOfType<MapDisplay> ();
+		
+		if (drawMode == DrawMode.NoiseMap) {
+			display.DrawTexture (TextureGenerator.TextureFromHeightMap (mapData.heightMap));
+		} else if (drawMode == DrawMode.Mesh) {
+			display.DrawMesh (MeshGenerator.GenerateTerrainMesh (mapData.heightMap, terrainData.meshHeightMultiplier, terrainData.meshHeightCurve, editorPreviewLOD, terrainData.useFlatShading));
+		} else if (drawMode == DrawMode.FalloffMap) {
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(FalloffGenerator.GenerateFalloffMap(mapChunkSize)));
+		}
+	}
+
+	// Generate map data : PerlinNoise map and colour map with all parameters
+	MapData GenerateMapData(Vector2 centre) {
+		float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize + 2, mapChunkSize + 2, noiseData.seed, noiseData.noiseScale, noiseData.octaves, noiseData.persistance, noiseData.lacunarity, centre + noiseData.offset, noiseData.normalizeMode);
+
+		if (terrainData.useFalloff)
+		{
+			if (falloffMap == null)
+			{
+				falloffMap = FalloffGenerator.GenerateFalloffMap (mapChunkSize+2);
+			}
+			for (int y = 0; y < mapChunkSize+2; y++) {
+				for (int x = 0; x < mapChunkSize+2; x++) {
+					if (terrainData.useFalloff) {
+						noiseMap [x, y] = Mathf.Clamp01(noiseMap [x, y] - falloffMap [x, y]);
+					}
+				}
+			}
+		}
+
+		return new MapData (noiseMap);
+	}
+
+	void OnValidate() {
+
+		if (terrainData != null)
+		{
+			terrainData.OnValuesUpdated -= OnValuesUpdated;
+			terrainData.OnValuesUpdated += OnValuesUpdated;
+		}
+		if (noiseData != null)
+		{
+			noiseData.OnValuesUpdated -= OnValuesUpdated;
+			noiseData.OnValuesUpdated += OnValuesUpdated;
+		}
+		if (textureData != null)
+		{
+			textureData.OnValuesUpdated -= OnTextureValuesUpdated;
+			textureData.OnValuesUpdated += OnTextureValuesUpdated;
+		}
+	}
+}
+
+[System.Serializable]
+
+
+public struct MapData {
+	public readonly float[,] heightMap;
+
+	public MapData (float[,] heightMap)
+	{
+		this.heightMap = heightMap;
+	}
+	
+}
--- Assets/Scripts/Objets/BlockCase.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BlockCase : MonoBehaviour
-{
-    public Vector2 tabRef;
-}
